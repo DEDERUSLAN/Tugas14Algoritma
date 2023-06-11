d0184c603c6982275511a241e6d25d03a23e961c
DEDERUSLAN <123922928+DEDERUSLAN@users.noreply.github.com>
2023-06-11 19:16:24 +0700

    Add files via upload

---
+++ ContohRekursi3.java
@@ -0,0 +1,16 @@
+public class ContohRekursi3 {
+    public static int power(int base, int exponent) {
+        if (exponent == 0) {
+            return 1;
+        } else {
+            return base * power(base, exponent - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int base = 2;
+        int exponent = 3;
+        int result = power(base, exponent);
+        System.out.println(base + " raised to the power of " + exponent + " is " + result);
+    }
+}
